3643627794dc36dd8b2aa55f4e22c1844a83fb92
DTSmth <ducktapedave85@gmail.com>
2025-11-01 09:24:34 -0400

    create function to read console input

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,3 +1,19 @@
+fn double(n: i32) -> i32 {
+    n * 2
+}
+
+fn greet(s: String) {
+    println!("{}", s);
+}
+
+fn read_line() -> String {
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).expect("Stdin not working");
+    input.trim().to_string()
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("type something");
+    let input = read_line();
+    println!("you typed : {}", input);
 }
