7f010df6d3e7d5413e4e9f66ae7fab12185a3bdb
DTSmth <ducktapedave85@gmail.com>
2025-11-01 17:02:42 -0400

    change login to use an array of users and match on them

--- authentication/src/lib.rs
+++ authentication/src/lib.rs
@@ -3,34 +3,52 @@ pub fn greet_user(name: &str) -> String {
 }
 
 pub fn login(username: &str, password: &str) -> Option<LoginAction> {
-    let username = username.to_lowercase();
-
-    if username != "admin" && username != "bob" {
-        return None;
+    let users = get_users();
+    let username = username.to_string();
+    if let Some(user) = users.iter().find(|user| user.username == username) {
+        if user.password == password {
+            return Some(LoginAction::Granted(user.role.clone()))
+        } else {
+            return Some(LoginAction::Denied)
+        }
     }
-
-    if username == "admin" && password == "password" {
-        Some(LoginAction::Granted(LoginRole::Admin))
-    } else if username == "bob" && password == "password" {
-        Some(LoginAction::Granted(LoginRole::User))
-    } else {
-        Some(LoginAction::Denied)
-    }
-
-    // username.to_lowercase() == "admin" && password == "password"
+    None
 }
 
-#[derive(PartialEq, Debug)]
+#[derive(PartialEq, Debug, Clone)]
 pub enum LoginAction {
     Granted(LoginRole),
     Denied,
 }
 
 
-#[derive(PartialEq, Debug)]
+#[derive(PartialEq, Debug, Clone)]
 pub enum LoginRole {
     Admin,
     User,
+}
+
+pub struct User {
+    pub username: String,
+    pub password: String,
+    pub role: LoginRole,
+}
+
+impl User {
+    pub fn new(username: &str, password: &str, role: LoginRole) -> User {
+        Self {
+            username: username.to_lowercase(),
+            password: password.to_string(),
+            role,
+        }
+    }
+}
+
+pub fn get_users() -> [User; 2] {
+    [
+        User::new("admin", "password", LoginRole::Admin),
+        User::new("bob", "password", LoginRole::User),
+    ]
 }
 
 pub fn read_line() -> String {
