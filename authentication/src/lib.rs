96b2c6036c809df2215cdc15faad7dbb94a10ff7
DTSmth <ducktapedave85@gmail.com>
2025-11-03 14:47:28 -0500

    add hashing to passwords

--- authentication/src/lib.rs
+++ authentication/src/lib.rs
@@ -1,7 +1,13 @@
 use std::collections::HashMap;
 use std::path::Path;
 use serde::{Serialize, Deserialize };
-use serde_json::json;
+
+pub fn hash_password(password: &str) -> String {
+    use sha2::Digest;
+    let mut hasher = sha2::Sha256::new();
+    hasher.update(password);
+    format!("{:X}", hasher.finalize())
+}
 
 pub fn greet_user(name: &str) -> String {
     format!("Hello, {name}!")
@@ -10,6 +16,7 @@ pub fn greet_user(name: &str) -> String {
 pub fn login(username: &str, password: &str) -> Option<LoginAction> {
     let users = get_users();
     let username = username.to_string();
+    let password = hash_password(password);
 
     if let Some(user) = users.get(&username) {
         if user.password == password {
@@ -56,7 +63,7 @@ impl User {
     pub fn new(username: &str, password: &str, role: LoginRole) -> User {
         Self {
             username: username.to_lowercase(),
-            password: password.to_string(),
+            password: hash_password(password),
             role,
         }
     }
