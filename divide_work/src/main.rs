84650411da20c7a5ce38256929014dc82c1be3b6
DTSmth <ducktapedave85@gmail.com>
2025-11-04 13:37:45 -0500

    dividing workloads with threads in rust

---
+++ divide_work/src/main.rs
@@ -0,0 +1,21 @@
+fn main() {
+    const N_THREADS: usize = 8;
+
+    let to_add: Vec<u32> = (0..5000).collect();
+    let mut thread_handles = Vec::new();
+    let chunks = to_add.chunks(N_THREADS);
+
+    for chunk in chunks {
+        let my_chunck = chunk.to_owned();
+        thread_handles.push(std::thread::spawn(move || {
+            my_chunck.iter().sum::<u32>()
+        }));
+    }
+
+    // Sum of each chunk
+    let mut sum = 0;
+    for handle in thread_handles {
+        sum += handle.join().unwrap();
+    }
+    println!("Total sum is {}", sum);
+}
