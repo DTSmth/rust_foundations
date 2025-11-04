17aaa1d1af0c3db303ad60926b76af6c8377beae
DTSmth <ducktapedave85@gmail.com>
2025-11-04 11:34:12 -0500

    learning threads in rust

---
+++ hello/src/main.rs
@@ -0,0 +1,15 @@
+fn hello_thread(n: u32) {
+    println!("Hello, world from thread! {n}");
+}
+
+fn main() {
+    println!("Hello, world from main thread!");
+
+    let mut thread_handles = Vec::new();
+    for i in 0..5 {
+        let thread_handle = std::thread::spawn(move || hello_thread(i));
+        thread_handles.push(thread_handle);
+    }
+
+    thread_handles.into_iter().for_each(|h| h.join().unwrap());
+}
