d79d98e3dc9ddd5f82de77612da01176d92181fb
DTSmth <ducktapedave85@gmail.com>
2025-11-13 07:37:28 -0500

    thread priority

---
+++ thread_priorityy/src/main.rs
@@ -0,0 +1,33 @@
+use std::sync::atomic::{AtomicI32, Ordering};
+use thread_priority::*;
+
+static LOW_COUNT: AtomicI32 = AtomicI32::new(0);
+static MEDIUM_COUNT: AtomicI32 = AtomicI32::new(0);
+static HIGH_COUNT: AtomicI32 = AtomicI32::new(0);
+
+
+fn low_priority() {
+    set_current_thread_priority(ThreadPriority::Min).unwrap();
+    loop {
+        LOW_COUNT.fetch_add(1,Ordering::Relaxed);
+        std::thread::yield_now();
+    }
+}
+
+fn regular_priority() {
+    // set_current_thread_priority(ThreadPriority::Min).unwrap();
+    loop {
+        MEDIUM_COUNT.fetch_add(1,Ordering::Relaxed);
+        std::thread::yield_now();
+    }
+}
+
+
+
+fn main() {
+    std::thread::spawn(low_priority);
+    std::thread::spawn(regular_priority);
+    std::thread::sleep(std::time::Duration::from_secs(5));
+    println!("Low: {}", LOW_COUNT.load(Ordering::Relaxed));
+    println!("Med: {}", MEDIUM_COUNT.load(Ordering::Relaxed));
+}
