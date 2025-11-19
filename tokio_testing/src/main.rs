70143e5cff5ba10141a679399aaedd67a3ed8bb4
DTSmth <ducktapedave85@gmail.com>
2025-11-19 17:41:00 -0500

    unit testing with tokio

---
+++ tokio_testing/src/main.rs
@@ -0,0 +1,30 @@
+
+async fn double(n: u32) -> u32 {
+    n * 2
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn simple_test() {
+        assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn will_compile() {
+        let rt = tokio::runtime::Builder::new_current_thread().enable_all().build().unwrap();
+
+        assert_eq!(rt.block_on(double(2)), 4);
+    }
+
+    #[tokio::test(flavor = "multi_thread", worker_threads = 2)]
+    async fn tokio_test() {
+        assert_eq!(double(2).await, 4);
+    }
+}
+fn main() {
+    println!("Hello, world!");
+}
