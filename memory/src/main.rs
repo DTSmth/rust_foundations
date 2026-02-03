79d7b4d2b3af0fb6d32822f6f693245094311e09
DTSmth <ducktapedave85@gmail.com>
2026-02-03 15:54:18 -0500

    start learning unsafe rust

---
+++ memory/src/main.rs
@@ -0,0 +1,16 @@
+/// # Safety
+///
+/// This is unsafe because i made the function unsafe
+/// could also use a block
+unsafe fn my_fn() {
+
+}
+
+fn main() {
+    let my_vec = vec![1,2,3,4];
+    unsafe {
+        // if let Ok(value) = my_vec.get_unchecked(2) {
+        //
+        // }
+    }
+}
