6e4dea3d70d6dc15f8a32eac5b864ac923c8578e
DTSmth <ducktapedave85@gmail.com>
2026-02-06 15:54:39 -0500

    allocate memory with rust

--- memory/src/main.rs
+++ memory/src/main.rs
@@ -1,3 +1,5 @@
+use std::alloc::Layout;
+
 /// # Safety
 ///
 /// This is unsafe because i made the function unsafe
@@ -12,5 +14,17 @@ fn main() {
         // if let Ok(value) = my_vec.get_unchecked(2) {
         //
         // }
+    }
+}
+
+fn allocate_memory_with_rust() {
+    use std::alloc::{alloc, dealloc, Layout};
+
+    unsafe {
+        let layout = Layout::new::<u16>();
+        let ptr = alloc(layout);
+        *ptr = 42;
+        assert_eq!(42, *ptr);
+        dealloc(ptr, layout);
     }
 }
